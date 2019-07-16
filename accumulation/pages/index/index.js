041009883a8b26868c5fb0cf8989d1f8d986dd1f
Kevinlookriver <grj@bwg.art>
2019-07-16 17:44:22 +0800

    2019-07-16 17:44

--- accumulation/pages/index/index.js
+++ accumulation/pages/index/index.js
@@ -2,34 +2,34 @@ const app = getApp();
 
 Page({
   data: {
-    linkArray: []
+    mine_img: ['/images/1.jpg', '/images/2.jpg', '/images/3.jpg', '/images/4.jpg']
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function() {
+  onLoad: function () {
     console.log(app.config.url)
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
-})
+});
--- accumulation/pages/test/test.js
+++ accumulation/pages/test/test.js
@@ -2,9 +2,14 @@ const app = getApp();
 const utils = require('../../utils/util');
 
 Page({
-  data: {},
+  data: {
+    array: [15, 52, 36, 48]
+  },
 
   onLoad() {
-    console.log(utils.formatTime(new Date(),'date'));
+    console.log(utils.formatTime(new Date(), 'date'));
+    this.data.array.forEach((item) => {
+      console.log(item * 100)
+    })
   }
 });
