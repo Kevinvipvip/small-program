979034f57aaaa4757abe09024d074989350d32df
Kevin <grj@bwg.art>
2019-08-30 19:41:44 +0800

    2019-08-30 19:41

--- accumulation/pages/count-down/count-down.js
+++ accumulation/pages/count-down/count-down.js
@@ -56,26 +56,21 @@ Page({
   },
   countDownDate(timeStamp, fn, callback) {
     let time_stamp = timeStamp.toString()[12] ? timeStamp : timeStamp * 1000;
-    let now_time_stamp = new Date().getTime();//获取现在距离1970年的毫秒数
+    let now_time_stamp = new Date().getTime(); //获取现在距离1970年的毫秒数
 
     if (time_stamp - now_time_stamp > 0) {
-      let s = Math.floor((time_stamp - now_time_stamp) / 1000);//计算未来时间距离现在的秒数
-      let d = Math.floor(s / (60 * 60 * 24));//整数部分代表的是天；一天有24*60*60=86400秒
-      s %= 60 * 60 * 24;//余数代表剩下的秒数
-      let h = Math.floor(s / (60 * 60));//整数部分代表小时
+      let s = Math.floor((time_stamp - now_time_stamp) / 1000); //计算未来时间距离现在的秒数
+      let d = Math.floor(s / (60 * 60 * 24)); //整数部分代表的是天；一天有24*60*60=86400秒
+      s %= 60 * 60 * 24; //余数代表剩下的秒数
+      let h = Math.floor(s / (60 * 60)); //整数部分代表小时
       s %= 60 * 60;
-      let m = Math.floor(s / 60);//整数部分代表分钟数
-      s %= 60;//最后剩下的是秒数
+      let m = Math.floor(s / 60); //整数部分代表分钟数
+      s %= 60; //最后剩下的是秒数
 
       let new_date = [d, h, m, s].map(this.formatNumber);
       fn(new_date)
     } else {
       if (callback) {
-        wx.showToast({
-          title: '已结束',
-          icon: 'none',
-          duration: 2000
-        });
         callback()
       }
     }
